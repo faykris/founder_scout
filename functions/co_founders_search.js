270773183f874a8d8465270b043979c096d38413
faykris <faykris28@gmail.com>
2021-10-25 22:20:14 -0500

    [Update] co-founders_search, profiles_scraper.js [Add] insert_profile.js

--- functions/co_founders_search.js
+++ functions/co_founders_search.js
@@ -6,9 +6,9 @@ export default async function co_founders_search(p_list, geoId, count) { // filt
     "q=savedSearch&" +
     "savedSearchId=50541275&" +
     "newResultsOnly=true&" +
-    "start=" + String(count) + "&" +
-    "count=100&" +
-    "lastViewedAt=" + String(Date.now()).substr(0,10), {
+    "start="+ String(count) +"&" +
+    "count=25&" +
+    "lastViewedAt=1635184707516", {
     "headers": {
       "accept": "*/*",
       "accept-language": "es-ES,es;q=0.9",
@@ -21,9 +21,9 @@ export default async function co_founders_search(p_list, geoId, count) { // filt
       "sec-fetch-site": "same-origin",
       "x-li-identity": "dXJuOmxpOm1lbWJlcjo5Mzc3NDc3ODk",
       "x-li-lang": "en_US",
-      "x-li-page-instance": "urn:li:page:d_sales2_search_people_pivot_saved_delta;DUeYU55sRCyapwh3ofLLbw==",
+      "x-li-page-instance": "urn:li:page:d_sales2_search_people_pivot_saved_delta;1OSzaTOdTkCdGJ+uVEzzhw==",
       "x-restli-protocol-version": "2.0.0",
-      "cookie": "li_sugr=248669f8-8a6c-4bfe-a600-5635dcdb011d; bcookie=\"v=2&61e76a31-c046-4d98-8c2c-8ee2f447df62\"; bscookie=\"v=1&20211006191210caa845ea-ad33-4bc8-8fc3-fb97e9f42056AQGf7FqLvk94GsVBhKyiNfSBpdHzOcOE\"; fcookie=AQGDRerWJbatZgAAAXyJerSofe5fdTVf2dbwSpfdfKkgBN_K-lRRo-cUMCKG47PM4aviCJC89rPSUA3cxdVmMw3VyUrICh6RHod2WENHc0hVJ6xc6ddy3ZDTzePmurOuO8Sn6zW82mtDy6H5pv6bNalNm-XOyrWxkbmUHflIlUOFJAf9J6hK3VVbwSv7y2i2G-99vsK6LyuZcnpCSug8j0eXD0LGr3A2MnseSMUpVV1YrDEco7Dl4E8ZGpU0a-mn-seLDAgxb8+78WAjtyX/5ptVxVAqArCVJY6rcQkAmPD2nwttDVZW5eqevyw1I+6Qo4OcYfQY+R3SDTGsRFXBAc/zDHlg==; li_at=AQEDATfk5U0AwT9rAAABfIl6tR0AAAF8rYc5HU0AQ-SzGCo_6E1piIH2HHTLdwP3DH2_Y9_JGTyKwyRx8zgWIrQ526uPWi8MQksBF0p99ejug3p2VNwU2u-pUBLGd7pbDELSA19HK5F9Z8gj-F-mWIHY; liap=true; JSESSIONID=\"ajax:3412942423165963119\"; timezone=America/Bogota; _guid=9eb18b37-19a5-42f0-a5ba-dadba388e031; aam_uuid=41029341209754532414172781260508165292; AMCV_14215E3D5995C57C0A495C55%40AdobeOrg=-637568504%7CMCIDTS%7C18920%7CMCMID%7C40531383935555829624118433342738401127%7CMCAAMLH-1635268010%7C4%7CMCAAMB-1635268010%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1634670410s%7CNONE%7CMCCIDH%7C1202759587%7CvVersion%7C5.1.1; AnalyticsSyncHistory=AQLWmDur7oHzLwAAAXyozaJy0cdhEsjZ64JDWeLnEa6vQUi6EgqG8L-n3FsabnLmzbFvrwv9mIQI7uH8ErYDRg; lms_ads=AQHNNwUeh46lsAAAAXyozaMlC4He5JJwKk7vrskLZhZJ_U4WaKfj67fWUmBbvGOm8jyJ-8WCO50hNNDnQrjlQTAsxZbexLfH; lms_analytics=AQHNNwUeh46lsAAAAXyozaMlC4He5JJwKk7vrskLZhZJ_U4WaKfj67fWUmBbvGOm8jyJ-8WCO50hNNDnQrjlQTAsxZbexLfH; fid=AQGAyoYGU453ZwAAAXyp75DDLTJtCUZSLpWj9ocr8tcy06ItiZXuj5l6a1Icw0FYFanx8ZqzOhIQvA; lang=v=2&lang=en-us; UserMatchHistory=AQJzViivZZWGvAAAAXyp75iBpMCjZhIKT19azTdlYd9E8NpNY0kvJglF-ZBnVDcZcHYPfdUe8qYfvWiNoau_TT81fupr6Bdg27KrUcys8dTOxcRrwKfjsJyxJH6P7LSIDi0ybnznXTDAvg9mmFisBbFHqU9AvVG5rtD2sH1Y1_ONrDO7Nkus4F4gMwzJVzNwzKk4UQA5xnGKOlGWAPvdYDG1MXff1sLEJIVRvb0f9fwYPeOhm_y-mjTnHQBWJAWgVDYzzYXWipe21nom98woxk7Tt5KFOfdGHj4ixFU; lidc=\"b=TB89:s=T:r=T:a=T:p=T:g=2670:u=7:x=1:i=1634938625:t=1635025022:v=2:sig=AQHG7f-axXZ3qMU3iaMDw0CJaxGXLaPB\"; li_a=AQJ2PTEmc2FsZXNfY2lkPTk2ODk1MjcwNyUzQSUzQTMxMTkwNzgwNwTuBj_cbTq3QpkUpmWQ_Xy_nKg2"
+      "cookie": "li_sugr=248669f8-8a6c-4bfe-a600-5635dcdb011d; bcookie=\"v=2&61e76a31-c046-4d98-8c2c-8ee2f447df62\"; bscookie=\"v=1&20211006191210caa845ea-ad33-4bc8-8fc3-fb97e9f42056AQGf7FqLvk94GsVBhKyiNfSBpdHzOcOE\"; liap=true; JSESSIONID=\"ajax:3412942423165963119\"; timezone=America/Bogota; _guid=9eb18b37-19a5-42f0-a5ba-dadba388e031; aam_uuid=41029341209754532414172781260508165292; AMCV_14215E3D5995C57C0A495C55%40AdobeOrg=-637568504%7CMCIDTS%7C18920%7CMCMID%7C40531383935555829624118433342738401127%7CMCAAMLH-1635268010%7C4%7CMCAAMB-1635268010%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1634670410s%7CNONE%7CMCCIDH%7C1202759587%7CvVersion%7C5.1.1; li_at=AQEDATfk5U0AwT9rAAABfIl6tR0AAAF815o2aU0AIxHDuRNuuCesLS_do46-QGea5RcyB2c1zxAtfYQPcuM28XdJQ7N0r8hN-5PyZCqCH7bPPOvtAm2BHoudEcASge2Ui5eGnDpfErVppWJ_cTHoBIEa; fid=AQE37cJpm_6i0wAAAXy32YFX3A0eQogTeqwy6EvzFIwHkj6Y0nJe0fbRD1lo2cqjuiJfdTleO2lToA; lang=v=2&lang=en-us; UserMatchHistory=AQLLpxEuYQXUSAAAAXy4qCPX-bBA3kdCBi9EW0amBNAzmgERnpoq82CCqCLFpZ0YZkJ6uudP2j3PtiuExytHntaAhnKq3AVE2jEIHcf4asCXkqUTaB-E6P87ULydpeeypFm7KSfbDBLLCVu8RjgPV_DnEimUaXfndpZPCVBe_GPIBjabYHzoa46kfbdl0g8SDh9sD02_bSDDhmBgBKuGGk7NfndgYQwfDgwmxFbtEkj4rVrhqaY2GBFZkE3qucBOLE81mfYhP3WmVAg9okaJA3c2V9ebl1diigF7o0I; AnalyticsSyncHistory=AQKta_UMIwTMqAAAAXy4qCPXM7IaXFRNbWR-ABE6QhcW_-bxpDLy1S0Gvu-CU6Zw9B444NixKGzuGVNfG5RBTg; lms_ads=AQH9YUKf5AmSYAAAAXy4qCVCrJBwdx2oNdH5Li-rhLBacp750ixfHa53FaCMjCVv38T6cjcZsFQtFzZTWcE30yYOgsxApTPX; lms_analytics=AQH9YUKf5AmSYAAAAXy4qCVCrJBwdx2oNdH5Li-rhLBacp750ixfHa53FaCMjCVv38T6cjcZsFQtFzZTWcE30yYOgsxApTPX; lidc=\"b=TB89:s=T:r=T:a=T:p=T:g=2671:u=7:x=1:i=1635185674:t=1635268925:v=2:sig=AQHXO5vTbdEf0B1p4SAsFPUPnj44D6ZI\"; li_a=AQJ2PTEmc2FsZXNfY2lkPTk2ODk1MjcwNyUzQSUzQTMxMTkwNzgwNwTuBj_cbTq3QpkUpmWQ_Xy_nKg2"
     },
     "referrer": "https://www.linkedin.com/sales/search/saved-searches/people",
     "referrerPolicy": "strict-origin-when-cross-origin",
@@ -31,11 +31,10 @@ export default async function co_founders_search(p_list, geoId, count) { // filt
     "method": "GET",
     "mode": "cors"
   });
-  console.log(String(Date.now()));
 
   const min = 5;
   const max = 30;
-  // salesApiPeopleSearch?q=peopleSearchQuery&start=0
+
   const cof_filter = await result.json();
   if (cof_filter.status > 399) {
     console.log("Error fetching data: status ", cof_filter.status)
@@ -82,7 +81,6 @@ export default async function co_founders_search(p_list, geoId, count) { // filt
     p_list.push(profileDict);
     count++;
   }
-
   console.log("Processed", count, "/", cof_filter.paging.total, "LinkedIn profiles");
   if (count >= cof_filter.paging.total) {
     return p_list;
--- profiles_scraper.js
+++ profiles_scraper.js
@@ -12,8 +12,8 @@ const min = 5;
 
 
 if (process.argv.length < 3) {
-    console.log("Usage: node [NODE_SCRIPT] [binGeo id] ...");
-    process.exit(1);
+  console.log("Usage: node [NODE_SCRIPT] [binGeo id] ...");
+  process.exit(1);
 }
 
 dotenv.config();
@@ -21,136 +21,136 @@ const base = new Airtable({apiKey: process.env.APY_KEY}).base(process.env.BASE_I
 
 // save binGeo Ids in a list
 for (let i = 2; process.argv[i]; i++) {
-    binGeoList.push(process.argv[i]);
+  binGeoList.push(process.argv[i]);
 }
 mongoose.connect(process.env.MONGODB_URI)
-    .then(async () => {
-        console.log("Connection successfully!");
+  .then(async () => {
+    console.log("Connection successfully!");
 
-        // Define default schema
-        const FounderSchema = mongoose.Schema(
-            {},
-            { strict: false }
-        );
+    // Define default schema
+    const FounderSchema = mongoose.Schema(
+      {},
+      { strict: false }
+    );
 
-        // Compile schema to model
-        const Cof = mongoose.model('Co-Founders', FounderSchema, 'co-founders');
-        for (let i = 0; binGeoList[i]; i++) {
-            let profile_list = [];
-            let success = 0;
-            let ignored = 0;
+    // Compile schema to model
+    const Cof = mongoose.model('Co-Founders', FounderSchema, 'co-founders');
+    for (let i = 0; binGeoList[i]; i++) {
+      let profile_list = [];
+      let success = 0;
+      let ignored = 0;
 
-            profile_list = await co_founders_search(profile_list, binGeoList[i], 0)
-                .catch((err) => { return err });
-            //console.log(profile_list);
-            if (profile_list.type === 'max-redirect') {
-                console.log("Max redirect reached - change profile header fetch info.");
-                break;
-            }
-            else if (profile_list.type !== undefined) {
-                console.log("Error fetching data: %s", profile_list.type);
-                continue;
-            }
+      profile_list = await co_founders_search(profile_list, binGeoList[i], 0)
+        .catch((err) => { return err });
+      //console.log(profile_list);
+      if (profile_list.type === 'max-redirect') {
+        console.log("Max redirect reached - change profile header fetch info.");
+        break;
+      }
+      else if (profile_list.type !== undefined) {
+        console.log("Error fetching data: %s", profile_list.type);
+        continue;
+      }
 
-            for (let j = 0; profile_list[j]; j++) {
-                try {
-                    const companyId = profile_list[j].companyId;
-                    const vmid = profile_list[j].vmid;
-                    let all_cof_info = { "profileInfo": {}, "companyInfo": { location: {}, createdAt: {} } };
+      for (let j = 0; profile_list[j]; j++) {
+        try {
+          const companyId = profile_list[j].companyId;
+          const vmid = profile_list[j].vmid;
+          let all_cof_info = { "profileInfo": {}, "companyInfo": { location: {}, createdAt: {} } };
 
-                    const time = (Math.random() * (max - min) + min).toFixed();
-                    if (companyId !== undefined) {
-                        const promise = Cof.findOne({ "profileInfo.vmid": vmid }).exec();
-                        assert.ok(promise instanceof Promise);
-                        await promise.then(async function (cof) {
-                            if (cof === null) {
-                                console.log('---> Scraping company information of %s in %d seconds... ', profile_list[j].fullname, time);
-                                await sleep(time * 1000);
-                                const companyInfo = await company_info(companyId).catch((err) => { return err });
-                                if (companyInfo.type !== 'max-redirect') {
-                                    const airtable_dict = {fields:{}};
-                                    const airtable_list = [];
-                                    // filling in profile info
-                                    airtable_dict.fields.vmid = all_cof_info.profileInfo.vmid = vmid;
-                                    airtable_dict.fields.profileUrl = all_cof_info.profileInfo.profileUrl = profile_list[j].profileUrl;
-                                    airtable_dict.fields.linkedInProfileUrl = all_cof_info.profileInfo.linkedInProfileUrl = profile_list[j].linkedInProfileUrl;
-                                    airtable_dict.fields.firstName = all_cof_info.profileInfo.firstName = profile_list[j].firstName.trim();
-                                    airtable_dict.fields.lastName = all_cof_info.profileInfo.lastName = profile_list[j].lastName.trim();
-                                    airtable_dict.fields.fullName = all_cof_info.profileInfo.fullName = profile_list[j].fullname.trim();
-                                    airtable_dict.fields.title = all_cof_info.profileInfo.title = profile_list[j].title.trim();
-                                    airtable_dict.fields.location = all_cof_info.profileInfo.location = profile_list[j].location.trim();
-                                    airtable_dict.fields.isPremium = all_cof_info.profileInfo.isPremium= profile_list[j].premium;
-                                    airtable_dict.fields.profileImageUrl = all_cof_info.profileInfo.profileImageUrl = profile_list[j].profileImageUrl;
-                                    // filling in company info
-                                    airtable_dict.fields.companyId = all_cof_info.companyInfo.companyId = profile_list[j].companyId;
-                                    airtable_dict.fields.companyUrl = all_cof_info.companyInfo.companyUrl= profile_list[j].companyUrl;
-                                    if (companyInfo.website !== undefined) {
-                                        airtable_dict.fields.websiteUrl = all_cof_info.companyInfo.websiteUrl = companyInfo.website;
-                                    }
-                                    airtable_dict.fields.companyName = all_cof_info.companyInfo.companyName = profile_list[j].companyName;
-                                    airtable_dict.fields.industry = all_cof_info.companyInfo.industry = profile_list[j].industry || companyInfo.industry;
-                                    if (companyInfo.headquarters !== undefined) {
-                                        airtable_dict.fields.country = all_cof_info.companyInfo.location.country = companyInfo.headquarters.country.trim();
-                                        airtable_dict.fields.city = all_cof_info.companyInfo.location.city = companyInfo.headquarters.city.trim();
-                                    }
-                                    airtable_dict.fields.month = all_cof_info.companyInfo.createdAt.month = profile_list[j].month;
-                                    airtable_dict.fields.year = all_cof_info.companyInfo.createdAt.year = profile_list[j].year;
-                                    if (companyInfo.companyPictureDisplayImage !== undefined) {
-                                        airtable_dict.fields.companyImageUrl = all_cof_info.companyInfo.companyImageUrl = companyInfo.companyPictureDisplayImage.rootUrl +
-                                          companyInfo.companyPictureDisplayImage.artifacts[0].fileIdentifyingUrlPathSegment;
-                                    }
-                                    all_cof_info.companyInfo.summary = profile_list[i].summary;
-                                    airtable_dict.fields.timestamp = all_cof_info.companyInfo.timestamp = new Date().toISOString();
+          const time = (Math.random() * (max - min) + min).toFixed();
+          if (companyId !== undefined) {
+            const promise = Cof.findOne({ "profileInfo.vmid": vmid }).exec();
+            assert.ok(promise instanceof Promise);
+            await promise.then(async function (cof) {
+              if (cof === null) {
+                console.log('---> Scraping company information of %s in %d seconds... ', profile_list[j].fullname, time);
+                await sleep(time * 1000);
+                const companyInfo = await company_info(companyId).catch((err) => { return err });
+                if (companyInfo.type !== 'max-redirect') {
+                  const airtable_dict = {fields:{}};
+                  const airtable_list = [];
+                  // filling in profile info
+                  airtable_dict.fields.vmid = all_cof_info.profileInfo.vmid = vmid;
+                  airtable_dict.fields.profileUrl = all_cof_info.profileInfo.profileUrl = profile_list[j].profileUrl;
+                  airtable_dict.fields.linkedInProfileUrl = all_cof_info.profileInfo.linkedInProfileUrl = profile_list[j].linkedInProfileUrl;
+                  airtable_dict.fields.firstName = all_cof_info.profileInfo.firstName = profile_list[j].firstName.trim();
+                  airtable_dict.fields.lastName = all_cof_info.profileInfo.lastName = profile_list[j].lastName.trim();
+                  airtable_dict.fields.fullName = all_cof_info.profileInfo.fullName = profile_list[j].fullname.trim();
+                  airtable_dict.fields.title = all_cof_info.profileInfo.title = profile_list[j].title.trim();
+                  airtable_dict.fields.location = all_cof_info.profileInfo.location = profile_list[j].location.trim();
+                  airtable_dict.fields.isPremium = all_cof_info.profileInfo.isPremium= profile_list[j].premium;
+                  airtable_dict.fields.profileImageUrl = all_cof_info.profileInfo.profileImageUrl = profile_list[j].profileImageUrl;
+                  // filling in company info
+                  airtable_dict.fields.companyId = all_cof_info.companyInfo.companyId = profile_list[j].companyId;
+                  airtable_dict.fields.companyUrl = all_cof_info.companyInfo.companyUrl= profile_list[j].companyUrl;
+                  if (companyInfo.website !== undefined) {
+                    airtable_dict.fields.websiteUrl = all_cof_info.companyInfo.websiteUrl = companyInfo.website;
+                  }
+                  airtable_dict.fields.companyName = all_cof_info.companyInfo.companyName = profile_list[j].companyName;
+                  airtable_dict.fields.industry = all_cof_info.companyInfo.industry = profile_list[j].industry || companyInfo.industry;
+                  if (companyInfo.headquarters !== undefined) {
+                    airtable_dict.fields.country = all_cof_info.companyInfo.location.country = companyInfo.headquarters.country.trim();
+                    airtable_dict.fields.city = all_cof_info.companyInfo.location.city = companyInfo.headquarters.city.trim();
+                  }
+                  airtable_dict.fields.month = all_cof_info.companyInfo.createdAt.month = profile_list[j].month;
+                  airtable_dict.fields.year = all_cof_info.companyInfo.createdAt.year = profile_list[j].year;
+                  if (companyInfo.companyPictureDisplayImage !== undefined) {
+                    airtable_dict.fields.companyImageUrl = all_cof_info.companyInfo.companyImageUrl = companyInfo.companyPictureDisplayImage.rootUrl +
+                      companyInfo.companyPictureDisplayImage.artifacts[0].fileIdentifyingUrlPathSegment;
+                  }
+                  all_cof_info.companyInfo.summary = profile_list[i].summary;
+                  airtable_dict.fields.timestamp = all_cof_info.companyInfo.timestamp = new Date().toISOString();
 
-                                    // Parse to Int company info for Airtable
-                                    airtable_dict.fields.companyId = parseInt(airtable_dict.fields.companyId);
-                                    // Parse to date with only YYYY-MM-DD for Airtable
-                                    airtable_dict.fields.timestamp = airtable_dict.fields.timestamp.substr(0,10);
+                  // Parse to Int company info for Airtable
+                  airtable_dict.fields.companyId = parseInt(airtable_dict.fields.companyId);
+                  // Parse to date with only YYYY-MM-DD for Airtable
+                  airtable_dict.fields.timestamp = airtable_dict.fields.timestamp.substr(0,10);
 
-                                    // save profile in Airtable
-                                    airtable_list.push(airtable_dict);
-                                    await base('co-founders')
-                                      .create(airtable_list)
-                                      .then(async (record) => console.log(":D - %s from %s saved in Airtable.", profile_list[j].fullname, profile_list[j].companyName))
-                                      .catch(async (err) => console.log("X( - Error saving profile in Airtable:\n", err.message));
+                  // save profile in Airtable
+                  airtable_list.push(airtable_dict);
+                  await base('co-founders')
+                    .create(airtable_list)
+                    .then(async (record) => console.log(":D - %s from %s saved in Airtable.", profile_list[j].fullname, profile_list[j].companyName))
+                    .catch(async (err) => console.log("X( - Error saving profile in Airtable:\n", err.message));
 
-                                    // save profile in MongoDB Atlas
-                                    const co_founder = new Cof(all_cof_info);
-                                    const promise = co_founder.save();
-                                    assert.ok(promise instanceof Promise);
+                  // save profile in MongoDB Atlas
+                  const co_founder = new Cof(all_cof_info);
+                  const promise = co_founder.save();
+                  assert.ok(promise instanceof Promise);
 
-                                    await promise.then(async function (doc) {
-                                        console.log(":D - %s from %s saved in the co-founders collection.", profile_list[j].fullname, profile_list[j].companyName);
-                                        success++;
-                                    });
-                                    await promise.catch(async (err) => console.log(err));
-                                }
-                                else {
-                                    console.log("Max redirect reached - change company header fetch info");
-                                    //break;
-                                }
-                            }
-                            else {
-                                console.log(":| - %s from %s already exists in the collection. Ignored.", profile_list[j].fullname, profile_list[j].companyName);
-                                ignored++;
-                            }
-                        });
-                    }
-                    else {
-                        console.log(":( - %s from %s doesn't have companyId. Ignored.", profile_list[j].fullname, profile_list[j].companyName);
-                        ignored++;
-                    }
+                  await promise.then(async function (doc) {
+                    console.log(":D - %s from %s saved in the co-founders collection.", profile_list[j].fullname, profile_list[j].companyName);
+                    success++;
+                  });
+                  await promise.catch(async (err) => console.log(err));
                 }
-                catch (exe) {
-                    console.error("X( - failed processing company information %s from %s. Ignored.", profile_list[j].fullname, profile_list[j].companyName)
-                    console.error(exe);
+                else {
+                  console.log("Max redirect reached - change company header fetch info");
+                  //break;
                 }
-              console.log("* Profiles processed: %d/%d saved: %d ignored: %d", j + 1, profile_list.length, success, ignored);
-            }
+              }
+              else {
+                console.log(":| - %s from %s already exists in the collection. Ignored.", profile_list[j].fullname, profile_list[j].companyName);
+                ignored++;
+              }
+            });
+          }
+          else {
+            console.log(":( - %s from %s doesn't have companyId. Ignored.", profile_list[j].fullname, profile_list[j].companyName);
+            ignored++;
+          }
         }
-        mongoose.disconnect().then(() => console.log("Connection Closed"));
-    })
-    .catch((err) => console.log("Connection failed!\n", err))
+        catch (exe) {
+          console.error("X( - failed processing company information %s from %s. Ignored.", profile_list[j].fullname, profile_list[j].companyName)
+          console.error(exe);
+        }
+        console.log("* Profiles processed: %d/%d saved: %d ignored: %d", j + 1, profile_list.length, success, ignored);
+      }
+    }
+    mongoose.disconnect().then(() => console.log("Connection Closed"));
+  })
+  .catch((err) => console.log("Connection failed!\n", err))
 
 // (id:105015875),(id:101174742),(id:105080838),(id:101452733)
 /*  fetch("https://www.linkedin.com/sales-api/salesApiPeopleSearch?" +
