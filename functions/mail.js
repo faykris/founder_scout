371d6a3c31bb03ec3de1e55d327cda95900134ee
faykris <faykris28@gmail.com>
2021-10-28 20:38:24 -0500

    [Update] mail.js, send_email.js: add email send for new profiles and max redirection errors

--- functions/mail.js
+++ functions/mail.js
@@ -1,19 +1,21 @@
-/*const nodemailer = require("nodemailer");*/
+import dotenv from 'dotenv';
 import nodemailer from "nodemailer";
 
+dotenv.config();
+
 // create reusable transporter object using the default SMTP transport
-export const transporter = nodemailer.createTransport({
+export const transporter = await nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true, // true for 465, false for other ports
   auth: {
-    user: 'angiedesarrollo@gmail.com', // generated ethereal user
-    pass: 'pxioetehuggubpva', // generated ethereal password
+    user: process.env.EMAIL_USER, // generated ethereal user
+    pass: process.env.EMAIL_PASS, // generated ethereal password
   },
 });
 
-transporter.verify().then(() => (
+await transporter.verify().then(() => (
   console.log('Ready for send emails')
 ));
 
-export default transporter
+export default transporter;
